b472765acc402696e3a4f87ae40fa0be0cbf942b
Alex Lovelock <kloakk2@gmail.com>
2025-04-23 22:46:00 +0100

    feat: scroller

---
+++ index.js
@@ -0,0 +1,8 @@
+import EmblaCarousel from 'embla-carousel'
+import Autoplay from 'embla-carousel-autoplay'
+
+const emblaNode = document.querySelector('.embla')
+const options = { loop: true }
+const plugins = [Autoplay({ delay: 3000, stopOnInteraction: false })]
+
+const emblaApi = EmblaCarousel(emblaNode, options, plugins)
