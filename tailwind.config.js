d39d2279d5c6dc293276c21f064f61b1924bea65
Alex Lovelock <kloakk2@gmail.com>
2025-06-01 12:15:26 +0100

    feat: cleanly and properly implemented responsive text sizes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'fluid-base': 'clamp(0.95rem, 0.7vw, 1.2rem)',   // leave this — body text is fine
+        'fluid-lg':   'clamp(1.05rem, 0.8vw, 1.4rem)',   // leave this — stats look good
+        'fluid-xl':   'clamp(1.25rem, 1.5vw, 2.5rem)',   // FINAL — heading looks good on 1440p
+      },
+    }
   },
   plugins: [],
 }
