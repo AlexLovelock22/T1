d93dde700a4e0b514bc62065fc4c1827ca168e61
Alex Lovelock <kloakk2@gmail.com>
2025-06-06 17:03:04 +0100

    feat: responsive index for small and large screens.
    Mobile layout remains.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,19 @@ module.exports = {
   theme: {
     extend: {
       fontSize: {
-        'fluid-base': 'clamp(0.7rem, 0.85vw, 1.2rem)',   // ~11.2px min
-        'fluid-lg':   'clamp(0.8rem, 1vw, 1.4rem)',      // ~12.8px min
-        'fluid-xl':   'clamp(1rem, 1.75vw, 2.5rem)',     // ~16px min
+        'fluid-base': 'clamp(0.7rem, 0.85vw, 1.2rem)',  
+        'fluid-md': 'clamp(0.75rem, 1vw, 1.4rem)', 
+        'fluid-lg':   'clamp(0.8rem, 1vw, 1.9rem)',     
+        'fluid-xl':   'clamp(1rem, 1.75vw, 2.5rem)',    
+      },
+      width: {
+        'icon-fluid': 'clamp(1.5rem, 2vw, 2.25rem)', 
+      },
+      height: {
+        'icon-fluid': 'clamp(1.5rem, 2vw, 2.25rem)',
+      },
+      screens: {
+        'max-1366': { max: '1570px' }, // matches your CSS logic
       },
     }
     
